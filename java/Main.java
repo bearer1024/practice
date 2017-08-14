bbcb70b484734dbd7cb4b400366f10f1c6c86d3c
bearer1024 <bearer1024@gmail.com>
2017-08-14 15:44:35 +0800

    add java part

---
+++ java/Main.java
@@ -0,0 +1,12 @@
+/**
+ *this file is used to test asseration in java
+ *@date 2017.09.02
+ */
+class Main{
+    public static void main(String []args){
+	int a = 1000;
+	String greeting = "hello bearer";
+	assert a>1:a;
+	assert greeting != null : "here has a greeting ";
+    }
+}
---
+++ java/TestArray.java
@@ -0,0 +1,20 @@
+/**
+ *to learn the usage of Array creation and initialization
+*/
+class TestArray {
+    public static void main(String [] args){
+	//create an array which the length is 5
+
+	int [] arrayOfInt = {1,2,5,6,1};
+	//one step to creat
+	String [] hello = new String[3];
+
+	//creating and initialing in one step
+	int [] one2five = {1,2,3,4,5};
+
+	//print
+	for(int i=0; i<arrayOfInt.length; i++){
+	    System.out.println("the value of arrayOfInt is: "+arrayOfInt[i]);
+	}
+    }
+}
