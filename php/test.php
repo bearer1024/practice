3eccd03ab857c03f3ca5b04fe5596aa2338e66ff
bearer1024 <bearer1024@gmail.com>
2017-09-24 23:56:13 +0800

    add php part to this repository

---
+++ php/test.php
@@ -0,0 +1,15 @@
+<?php
+
+//test print multiple lines
+print "hello
+MR.
+	Tiger
+";
+
+//test heredoc
+echo <<<HELLO
+The "Tiger" is my lovely catty.
+I spent $100 to buy a gift today.
+I don't neet any escape character to output double quote symbol and other symbol.
+HELLO;
+	?>
