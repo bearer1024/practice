a9b7329a5fb9a1c31b61401f8f50cbdf1c5bab8d
bearer1024 <bearer1024@gmail.com>
2017-09-25 01:01:57 +0800

    STATIC not means that can be allowed to invoker externally

--- php/test.php
+++ php/test.php
@@ -10,6 +10,20 @@ MR.
 echo <<<HELLO
 The "Tiger" is my lovely catty.
 I spent $100 to buy a gift today.
+/n also doesn't work at here.
 I don't neet any escape character to output double quote symbol and other symbol.
 HELLO;
-	?>
+echo"\n";
+
+//test static
+function countSheep(){
+STATIC $count = 0;
+$count++;
+print"There have $count sheeps now\n";
+}
+
+countSheep();
+countSheep();
+countSheep();
+
+?>
